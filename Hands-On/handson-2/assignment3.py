9ab12afff83653a7462e92b5cbefacd639e4d1ad
tisanbako <bakotisan@gmail.com>
2023-09-19 01:07:38 -0400

    updating assignment2

--- Hands-On/handson-2/assignment3.py
+++ Hands-On/handson-2/assignment3.py
@@ -1,10 +1,10 @@
 email = "tisan.bako@bellmedia.ca"
 new_email = email.split("@") #store "['tisan.bako', 'bellmedia.ca']" in a variable new_email
 print(new_email)  #['tisan.bako', 'bellmedia.ca']
-print(new_email[0]) #bellmedia.ca
+print(new_email[0]) #tisan.bako
 print(type(new_email[1]))  #<class 'str'>
 
-#to futher seprate new_email = bellmedia.ca
+#to futher seprate new_email = bellmedia.ca[1]
 
 email2 = new_email[1]  #store "bellmedia.ca" in a variable
 print(email2)
@@ -14,4 +14,6 @@ print(email3) #['bellmedia', 'ca']
 
 company_name =email3[0]
 print(company_name)  #bellmedia
-print(email.split("@"))
+
+#we can achieve all that with this one line 
+# print(email.split("@")[1].split(".")[0])
