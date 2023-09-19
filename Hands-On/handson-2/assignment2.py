26106a501be432178f659952bd88c80f6471cc1c
tisanbako <bakotisan@gmail.com>
2023-09-19 00:45:34 -0400

    adding assignment file

--- Hands-On/handson-2/assignment2.py
+++ Hands-On/handson-2/assignment2.py
@@ -6,6 +6,9 @@ emails = ["ironman@accenture.com",
             ]
 #to access an email we use index, to access all emails one at a time we use loop function
 
+#lets say we are looking for the value of company names from the emails above
+company_name = []
+
 for i in emails:  #i could be anything 
     print(i)
     emails2 = i.split("@") #this will apply for all emails as for loop function works on one email at a time 
@@ -17,7 +20,7 @@ for i in emails:  #i could be anything
     company_name = domain_name[0]
     print(company_name)
 
-
+#the print lines not necessary
 
 
 
