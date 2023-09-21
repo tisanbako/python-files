7e859ffa87622399946ae46dc66c4ae5991ed13d
tisanbako <bakotisan@gmail.com>
2023-09-21 13:36:23 -0400

    updating if statement file

--- Hands-On/handson-2/handson-if.py
+++ Hands-On/handson-2/handson-if.py
@@ -2,7 +2,7 @@ time = 6
 
 if time == 7:    #if time is equal 7
     print("time to join the class")
-print("sleep")   #this is a different code from the if statement
+print("sleep")   #this is a different code block from the if statement
 
 age = 20
 if age >= 18:
