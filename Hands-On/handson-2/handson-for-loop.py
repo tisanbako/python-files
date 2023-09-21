ba09506ab5748564eba62830cc51db2c1a7f6268
tisanbako <bakotisan@gmail.com>
2023-09-21 16:34:52 -0400

    updating for loop

--- Hands-On/handson-2/handson-for-loop.py
+++ Hands-On/handson-2/handson-for-loop.py
@@ -11,5 +11,14 @@ for fruit in fruits:
     new_value =  fruit.upper()
     total = 255 * 188
     print(total)
+
+ # to print number 1-10 for loop
+ 
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+for tisan in numbers:
+    print(tisan)
+
+
     
 
