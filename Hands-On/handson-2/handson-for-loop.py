fb6a2b467378e57f0f430fdeaabe93394d919101
tisanbako <bakotisan@gmail.com>
2023-09-21 16:29:06 -0400

    updating handson file

---
+++ Hands-On/handson-2/handson-for-loop.py
@@ -0,0 +1,15 @@
+name = "ironman"
+
+for x in name:
+    print(x)
+
+fruits = ["apple", "kiwi", "grapes", "mango",]
+
+
+for fruit in fruits:
+    print(fruit)
+    new_value =  fruit.upper()
+    total = 255 * 188
+    print(total)
+    
+
