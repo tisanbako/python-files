7a646d13f0ffa55fe8e3b7fed5e49bf40177a782
tisanbako <bakotisan@gmail.com>
2023-09-21 14:04:08 -0400

    updating assignmnet 2

--- Hands-On/handson-2/assignment2.py
+++ Hands-On/handson-2/assignment2.py
@@ -12,6 +12,7 @@ company_name = []
 for i in emails:  #i could be anything 
     print(i)
     emails2 = i.split("@") #this will apply for all emails as for loop function works on one email at a time 
+    print(emails2)
     domain = emails2[1]
     domain_name = domain.split(".")
     print(domain_name) #can take off this line.. print is just to see output
@@ -19,5 +20,5 @@ for i in emails:  #i could be anything
     print(company_name)
 
 #we can print all this in one line instead of the many line 13-19
-    company_name.append(i.split("@")[1].split(",")[0])
-    print(company_name)
+    # company_name.append(i.split("@")[1].split(",")[0])
+    # print(company_name)
