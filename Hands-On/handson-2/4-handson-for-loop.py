735e307d0b9d970635a988de8f12cd0ae0fcaeb4
tisanbako <bakotisan@gmail.com>
2023-09-22 12:23:00 -0400

    updating handson files

---
+++ Hands-On/handson-2/4-handson-for-loop.py
@@ -0,0 +1,33 @@
+name = "ironman"
+
+for x in name:
+    print(x)
+
+fruits = ["apple", "kiwi", "grapes", "mango",]
+
+
+for fruit in fruits:
+    print(fruit)
+    new_value =  fruit.upper()
+    total = 255 * 188
+    print(total)
+
+ # to print number 1-10 using for loop
+ 
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+for tisan in numbers:
+    print(tisan)
+
+#what if we want to print 1-100? that will be a long list of number value
+# best thing is to use range function
+
+for i in range(12): #1-11 .. it starts from i and end a number before 12 
+    print(i)
+
+for b in range(101): #1-100 .starts from 1 ends a number before 101
+    print(b)
+
+
+    
+
