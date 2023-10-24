552712179c4ce4c55f78d42f04b9eae36aaf8e33
tisanbako <bakotisan@gmail.com>
2023-10-24 16:18:46 -0400

    updating file

---
+++ Hands-On/handson-2/3-handson-api-getall.py
@@ -0,0 +1,53 @@
+import requests
+
+#to list users 
+#what if we want to call this many times? we creat a function 
+
+
+# response = requests.get("https://reqres.in/api/users?page=2")
+# print(response)
+# print(response.json())
+
+def get_users():
+    print("request: Getting all users....")
+    response = requests.get("https://reqres.in/api/users")  #remove ?page=2 from https://reqres.in/api/users?page=2 as it means that you want users in just page 2
+    print("ResponsCode: ", response)
+    print("responseBody: ", response.json())
+
+
+get_users()
+
+#this will print just what we have on top. but to get get the users and further 
+#process the data, we need to put it in a variable when calling it eg
+print()
+
+
+def get_users():
+    print("request: Getting all users....")
+    response = requests.get("https://reqres.in/api/users")  #remove ?page=2 from https://reqres.in/api/users?page=2 as it means that you want users in just page 2
+    #print("ResponsCode: ", response)
+    #print("responseBody: ", response.json())
+    return response.json()
+
+
+users = get_users()
+print(users)
+
+print()
+
+#we can further use the rerun object
+
+
+def get_users():
+    print("request: Getting all users....")
+    response = requests.get("https://reqres.in/api/users")  #remove ?page=2 from https://reqres.in/api/users?page=2 as it means that you want users in just page 2
+    #print("ResponsCode: ", response)
+    #print("responseBody: ", response.json())
+    return response.json()
+
+
+users = get_users()
+print(users)
+for i in users["data"]:   #data is the what the list?dict contains
+    print(i["email"], "sending email" )
+   
